fd471d91269ce52e7f6df62e38243a5ecbc996d6
Kevin Donahue <nonnontrivial@gmail.com>
2019-09-23 09:46:57 -0400

    add get intent lineage for message method

--- lib/file.ts
+++ lib/file.ts
@@ -1,4 +1,5 @@
 import * as utils from "@botmock-api/utils";
+// import uuid from "uuid/v4";
 import { writeFile, mkdirp } from "fs-extra";
 import { stringify as toYAML } from "yaml";
 import { EventEmitter } from "events";
@@ -111,8 +112,9 @@ export default class FileWriter extends EventEmitter {
               // case "generic":
               // case "list":
               default:
+                const value = message.payload[message.message_type];
                 type = "text";
-                payload = message.payload[message.message_type];
+                payload = typeof value !== "string" ? JSON.stringify(value) : value;
             }
             let value = payload ||
                 `${message.payload[message.message_type]
@@ -140,8 +142,7 @@ export default class FileWriter extends EventEmitter {
     const outputFilePath = join(this.outputDir, "domain.yml");
     return await writeFile(
       outputFilePath,
-      `# generated ${this.init}
-${toYAML({
+      `# generated ${this.init}${EOL}${toYAML({
         intents: this.projectData.intents.map(intent => intent.name),
         entities: this.projectData.entities.map(entity => entity.name),
         actions: this.getUniqueActionNames(),
@@ -163,21 +164,53 @@ ${toYAML({
     );
   }
   /**
+   * Gets the lineage of intents implied by a given message id
+   * @param messageId message id of a message connected by an intent
+   * @returns string[]
+   */
+  getIntentLineageForMessage(messageId: string): string[] {
+    const { getMessage, intentMap, projectData } = this;
+    const context: string[] = [];
+    const seenIds: string[] = [];
+    (function unwindFromMessageId(messageId: string) {
+      const { previous_message_ids: prevIds } = getMessage(messageId);
+      let messageFollowingIntent: any;
+      if ((messageFollowingIntent = prevIds.find(prev => intentMap.get(prev.message_id)))) {
+        const { name: nameOfIntent } = projectData.intents.find(intent => (
+          intent.id === intentMap.get(messageFollowingIntent.message_id)[0]
+        ));
+        if (typeof nameOfIntent !== "undefined") {
+          context.push(nameOfIntent);
+        }
+      } else {
+        for (const { message_id: prevMessageId } of prevIds) {
+          if (!seenIds.includes(prevMessageId)) {
+            seenIds.push(prevMessageId);
+            unwindFromMessageId(prevMessageId);
+          }
+        }
+      }
+    })(messageId);
+    return context;
+  }
+  /**
    * Write stories markdown file
    * @returns Promise<void>
    */
   private async writeStoriesFile(): Promise<void> {
-    const { project: { name: projectName }, board, intents } = this.projectData;
+    for (const messageId of this.intentMap.keys()) {
+      console.log(this.getIntentLineageForMessage(messageId));
+    }
     const outputFilePath = join(this.outputDir, "data", "stories.md");
     const storyData = {
-      intents,
+      intents: this.projectData.intents,
       intentMap: this.intentMap,
       messageCollector: this.messageCollector,
-      messages: board.board.messages
+      messages: this.projectData.board.board.messages
     };
     await writeFile(
       outputFilePath,
-      genStoriesFromIntents({ projectName, storyData })
+      genStoriesFromIntents({ projectName: this.projectData.project.name, storyData })
     )
   }
   /**
